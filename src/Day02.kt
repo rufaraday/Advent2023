e9c5f8d44581ecfe223fcd9fc5505361801a7669
Rafal Orlowski <rafal.orlowski@partner.bmw.de>
2023-12-02 15:06:19 +0100

    Day 2 initialization

--- src/Day02.kt
+++ src/Day02.kt
@@ -8,10 +8,10 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day02_test")
+    val testInput = readInput("Day01_test")
     check(part1(testInput) == 1)
 
-    val input = readInput("Day02")
+    val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
 }
